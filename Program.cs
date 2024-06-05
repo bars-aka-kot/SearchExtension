5526099696f97050139d93da83422caf96b442f6
Basirov Ilgiz <Basirov_ilgiz@mail.ru>
2024-06-05 11:11:49 +0500

    add gitignore

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SearchExtension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
