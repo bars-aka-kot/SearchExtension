35fd3079f0688d7d2d59af39da8ecf7b35f113da
Basirov Ilgiz <Basirov_ilgiz@mail.ru>
2024-06-06 01:53:24 +0500

    Add some code

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SearchFile.SearchExstension(args[0], args[1], args[2]);
+
+            foreach (var element in SearchFile.list)
+            {
+                Console.WriteLine(element);
+            }
         }
     }
 }
---
+++ SearchFile.cs
@@ -0,0 +1,42 @@
+﻿namespace SearchExtension
+{
+    public class SearchFile
+    {
+        public static List<string> list = new();
+        private static bool IsFoundedText(string path, string text)
+        {
+            using StreamReader sr = new StreamReader(path);
+            while (!sr.EndOfStream)
+            {
+                var tempString = sr.ReadLine();
+                if (tempString.Contains(text))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static void SearchExstension(string path, string extension, string text)
+        {
+            var files = Directory.GetFiles(path);
+
+            foreach (var file in files)
+            {
+                var name = Path.GetFileName(file);
+                var exten = Path.GetExtension(file);
+                if (exten == "." + extension)
+                {
+                    if (IsFoundedText(file, text))
+                    {
+                        list.Add(file);
+                    }
+                }
+            }
+            var directories = Directory.GetDirectories(path);
+            foreach (var directory in directories)
+            {
+                SearchExstension(directory, extension, text);
+            }
+        }
+    }
+}
